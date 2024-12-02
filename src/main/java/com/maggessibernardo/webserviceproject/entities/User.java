78c1310f4d7c033c55785e54735562f23f43b9ef
BernardoMaggessi <bermaggessi2@yahho.com>
2024-12-02 13:48:35 -0300

    h2 database, perfil de testes, JPA

--- src/main/java/com/maggessibernardo/webserviceproject/entities/User.java
+++ src/main/java/com/maggessibernardo/webserviceproject/entities/User.java
@@ -3,6 +3,14 @@ package com.maggessibernardo.webserviceproject.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 	//Serializable é utilizada para permitir que um objeto seja convertido em um fluxo de bytes, 
@@ -10,6 +18,8 @@ public class User implements Serializable {
 	//transmitido pela rede ou gravado em um banco de dados
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -17,8 +27,8 @@ public class User implements Serializable {
 	private String password;
 	
 	public User() {
-		
 	}
+	
 	public User(Long id,String name,String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
@@ -26,40 +36,52 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getPhone() {
 		return phone;
 	}
+	
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
