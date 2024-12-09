9b2653130b5360bcf48b6b880371fb09afb3ce24
BernardoMaggessi <bermaggessi2@yahho.com>
2024-12-09 12:38:53 -0300

    user insert

--- src/main/java/com/maggessibernardo/webserviceproject/resources/UserResource.java
+++ src/main/java/com/maggessibernardo/webserviceproject/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.maggessibernardo.webserviceproject.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.maggessibernardo.webserviceproject.entities.User;
 import com.maggessibernardo.webserviceproject.services.UserService;
@@ -30,6 +34,13 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	//FORMA ADEQUADO DE INSERIR UM OBJETO NO BANCO DE DADOS
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();//CRIANDO CAMINHO DE INSERÇÃO 201
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 	
